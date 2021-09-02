286e67b09e73f7a57fb7d2081773adf08c082865
sahil2501 <82953793+sahil2501@users.noreply.github.com>
2021-09-02 17:25:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDgQcCuUPaCPKsd0TeWMXygqUu9gXjpc9k",
+    authDomain: "kwitter-417cc.firebaseapp.com",
+    databaseURL: "https://kwitter-417cc-default-rtdb.firebaseio.com",
+    projectId: "kwitter-417cc",
+    storageBucket: "kwitter-417cc.appspot.com",
+    messagingSenderId: "154199866044",
+    appId: "1:154199866044:web:18bb513009d3c240472eb9"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  user_name = localStorage.getItem("user_name");
+
+  room_name = localStorage.getItem("room_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function login() {
+    user_name = document.getElementById("user_name").value;
+
+    firebase.database().ref("/").child(user_name).update({
+        purpose: "adding user"
+    });
+
+    localStorage.setItem("user_name", user_name);
+    window.location = "room.html";
+}
